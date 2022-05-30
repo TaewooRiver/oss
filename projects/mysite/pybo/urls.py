ae544e31be079e83efe10f9836f99fc1fd63fd05
Taewoo Kang <cooltwk@naver.com>
2022-05-30 22:33:16 +0000

    2022-05-31 morning

--- projects/mysite/pybo/urls.py
+++ projects/mysite/pybo/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'pybo'
+
 urlpatterns = [
-    path('', views.index), # comma 제발 붙입시다.
-    path('<int:question_id>/', views.detail),
+    path('', views.index, name='index'), # comma 제발 붙입시다.
+    path('<int:question_id>/', views.detail, name='detail'),
 ]
