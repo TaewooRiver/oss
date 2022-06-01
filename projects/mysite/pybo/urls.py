c7596add478e3bec6ba91fd6be5f25fbd38662c3
Taewoo Kang <cooltwk@naver.com>
2022-06-01 14:02:08 +0000

    2022-06-01 night

--- projects/mysite/pybo/urls.py
+++ projects/mysite/pybo/urls.py
@@ -7,4 +7,6 @@ app_name = 'pybo'
 urlpatterns = [
     path('', views.index, name='index'), # comma 제발 붙입시다.
     path('<int:question_id>/', views.detail, name='detail'),
+    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
+    
 ]
